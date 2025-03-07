e195e339a33b83716e719dbe0581a0abedd607aa
Tohir Kuliev <kuliev.tech@gmail.com>
2025-03-07 06:23:11 -0500

    Finish the js part

--- unit-converter/index.js
+++ unit-converter/index.js
@@ -1,3 +1,30 @@
 // 1 meter = 3.281 feet
 // 1 liter = 0.264 gallon
 // 1 kilogram = 2.204 pound
+
+const numberInputEl = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+
+const lengthEl = document.getElementById("length");
+const volumeEl = document.getElementById("volume");
+const massEl = document.getElementById("mass");
+
+convertBtn.addEventListener("click", function () {
+    const numberValue = numberInputEl.value;
+    const number = Number(numberValue);
+
+    if (!number) return;
+
+    const meterToFeet = (number * 3.281).toFixed(3);
+    const feetToMeter = (number / 3.281).toFixed(3);
+    const literToGallon = (number * 0.264).toFixed(3);
+    const gallonToLiter = (number / 0.264).toFixed(3);
+    const kiloToPounds = (number * 2.204).toFixed(3);
+    const poundsToKilos = (number / 2.204).toFixed(3);
+
+    lengthEl.textContent = `${number} meters =  ${meterToFeet} feet | ${number} feet = ${feetToMeter} meters`;
+
+    volumeEl.textContent = `${number} liters =  ${literToGallon} gallons | ${number} gallons = ${gallonToLiter} liters`;
+
+    massEl.textContent = `${number} kilos =  ${kiloToPounds} pounds | ${number} pounds = ${poundsToKilos} kilos`;
+});
